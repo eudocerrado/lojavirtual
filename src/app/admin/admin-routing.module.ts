c870f2befd801443d8c32bfd7aef9f0bf9e9ab8c
Laboratórios de Informática <labins@labin.canoas>
2018-06-11 21:44:47 -0300

    Falta o login

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './home/home.component';
 import {AdminComponent} from './admin/admin.component';
 import {PaginaNaoEncontradaComponent} from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 
 const routes: Routes = [
   {
     path: 'admin', component: AdminComponent, children: [
-      {path: '', component: HomeComponent},
+      {path: 'cadastro', component: CadastroComponent},
+      {path: 'inicio', component: InicioComponent},
       {path: '**', component: PaginaNaoEncontradaComponent}
       
       ,
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {AdminRoutingModule} from './admin-routing.module';
-import {HomeComponent} from './home/home.component';
 import {AdminComponent} from './admin/admin.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {SharedModule} from '../shared/shared.module';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 @NgModule({
@@ -21,9 +22,10 @@ import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao
     SharedModule
   ],
   declarations: [
-    HomeComponent,
     AdminComponent,
     PaginaNaoEncontradaComponent,
+    CadastroComponent,
+    InicioComponent,
     
     ],
   providers: [
